cb1bbedebed5edfcd0e3ae35515898c9cbb0b428
DiegoGV95 <diego.garcia.viana@taiger.com>
2019-02-19 13:49:57 +0100

    Update 06-missing-data.R

--- myscripts/Tema1/06-missing-data.R
+++ myscripts/Tema1/06-missing-data.R
@@ -8,3 +8,19 @@ is.na(data[4,2]) # Devueve TRUE porque en el dato original aparece un NA
 is.na(data[4,1])
 
 is.na(data$Income) # Comprobamos si hay algún NA en la columna 'Income' del dataset
+
+data.income.cleaned = data[!is.na(data$Income), ] # Devuelve todas las columnas, pero solo las filas donde 'Income' no es NA
+head(data.income.cleaned)
+
+complete.cases(data) # Devuelve TRUE si la fila no tiene NA (está completa) y FALSE en caso contrario
+
+data.cleaned.2 = data[complete.cases(data), ]
+head(data.cleaned.2, 10)
+
+# Convertir los ceros de ingresos en NA
+data$Income[data$Income == 0] = NA
+
+# Medidas de centralización y dispersión
+mean(data$Income, na.rm = TRUE) # Media
+sd(data$Income, na.rm = TRUE) # Desviación típica
+
