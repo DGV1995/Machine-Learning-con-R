e19334fd79c19c4bfae8bc26264e2cd318679596
DiegoGV95 <diego.garcia.viana@taiger.com>
2019-03-12 13:33:43 +0100

    Update 05-plots.R

--- myscripts/Tema2/05-plots.R
+++ myscripts/Tema2/05-plots.R
@@ -10,16 +10,25 @@ attach(auto) # Para ahorrarnos el poner todo el rato el símbolo '$'
 head(cylinders)
 
 # Histograma de frecuencias
-hist(acceleration, main = "Histograma de las aceleraciones", col = "blue",
+hist(acceleration, main = "Histograma de las aceleraciones", col = rainbow(12),
      xlab = "Aceleración", ylab = "Frecuencias", breaks = 16)
+
+hist(mpg, breaks = 16, prob = T)
+lines(density(mpg))
 
 # Boxplots
 boxplot(mpg, xlab = "Millas por Galeón")
 boxplot(mpg ~ model_year, xlab = "Millas por Galeón (por año)")
 boxplot(mpg ~ cylinders, xlab = "Consumo por número de cilindros")
+boxplot(auto)
 
 # Scatterplot
-plot(mpg ~ horsepower, xlab = "Caballos de potencia", ylab = "Millas por galeón")
+plot(mpg ~ horsepower, xlab = "Caballos de potencia", ylab = "Millas por galeón", type = "n") # n = none
+linearmodel = lm(mpg ~ horsepower)
+abline(linearmodel) # Recta de regresión
+
+# Agregar colores para cada cilindrada
+with(subset(auto, cylinders=="8cil"), points(horsepower, mpg, col = "red")) # De auto, coge los valores '8cil' de la columna 'cylinders'. Eje x = 'horsepower', Eje y = 'mpg'
 
 # Matriz de scartterplots
 pairs(~ mpg + displacement + horsepower + weight)
